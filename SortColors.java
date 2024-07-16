6a92f2db83829304d2d022c8a921321bdb152d5e
Mahak Pandey <116765334+mahakPandeyOfficial@users.noreply.github.com>
2024-07-16 21:29:43 +0530

    Create SortColors.java

---
+++ SortColors.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void sortColors(int[] nums) {
+      //sort the array
+        Arrays.sort(nums);
+      //according to sorting in order we will get 0 -> 1 -> 2 as colors we want as a output in questions
+        System.out.println(nums);
+    }
+}
